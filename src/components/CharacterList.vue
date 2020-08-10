a6b86dc110327a1799a38003e749b9b13110a7c7
r-tomiyama <r.tomiyama@opt.ne.jp>
2020-08-10 17:43:37 +0900

    画面ベース実装

---
+++ src/components/CharacterList.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-row>
+    <v-col>
+      <ul>
+        <li v-for="character of characters" :key="character.id">
+          {{ character.id }} {{ character.name }}
+        </li>
+      </ul>
+    </v-col>
+  </v-row>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "@vue/composition-api";
+
+export default defineComponent({
+  name: "CharacterList",
+  props: {
+    characters: {
+      default: []
+    }
+  },
+  setup() {
+    return {};
+  }
+});
+</script>
---
+++ src/pages/Characters.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-container>
+    <H1>キャラクター一覧</H1>
+    <CharacterList :characters="characters" />
+  </v-container>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "@vue/composition-api";
+import CharacterList from "../components/CharacterList.vue";
+
+export default defineComponent({
+  name: "Characters",
+  components: {
+    CharacterList
+  },
+  setup() {
+    const characters = [
+      { id: 1, name: "hoge" },
+      { id: 2, name: "fuga" }
+    ];
+    return {
+      characters
+    };
+  }
+});
+</script>
